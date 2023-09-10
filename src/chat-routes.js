be2bc86e6a09f6c11699909bc6258c46f3949527
Lucas Rocha <lr359508@gmail.com>
2023-09-10 12:24:37 -0300

    Progresso no chat.
    Agora é possível pesquisar por uma conta e iniciar uma conversa.

--- src/chat-routes.js
+++ src/chat-routes.js
@@ -1,7 +1,44 @@
 const router = require('express').Router()
 
+const mongoose = require('mongoose')
+const User = require('./user')
+
 router.get('/',(req,res)=>{
     res.render('profile',{ user: req.session.user })
+})
+
+router.post('/conversa', async (req,res)=>{
+    const nome = req.body.searchName
+
+    try {
+        console.log('Buscando por: ',nome)
+
+        const user = await User.findOne({
+            nome: nome
+        })
+        
+        console.log('Conta localizada: ',user.nome)
+        
+        if (user) {
+            res.redirect(`/chat/${user._id}`);
+        } else {
+            res.redirect('/chat');
+            res.alert(`Usuário '${nome}' não encontrado.`)
+        }
+    } catch (err) {
+        console.log(err)
+        res.redirect('/chat')
+    }
+})
+
+router.get('/:id', async (req,res)=>{
+    id = req.params.id
+
+    const user = await User.findOne({
+        _id: id
+    })
+
+    res.render('directmessage',{ pessoa: user })
 })
 
 module.exports = router
