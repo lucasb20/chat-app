8097ca8d2c8282a7efbe0d1fc826a1878c4c77bc
Lucas Rocha <lr359508@gmail.com>
2023-12-25 20:43:24 -0300

    Definição de endpoints de registrar usuário e adição de algumas configurações..

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,11 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-
+from authentication import views
 from django.urls import path
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
     path('verify/', TokenVerifyView.as_view()),
+    path('register/', views.register_user),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from authentication.serializers import UserSerializer
 
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    data = JSONParser().parse(request)
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
