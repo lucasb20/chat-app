89abcdcd7c2a2e94dc5caa42cdb2fdcbbcda34eb
Lucas Rocha <lr359508@gmail.com>
2024-02-16 16:56:01 -0300

    feat: Token model and Token serializer created

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from authentication.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
@@ -8,3 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = '__all__'
