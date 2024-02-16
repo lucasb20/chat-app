d34b3de20f18e60e5864ee6a778ffddd6d5c5faf
Lucas Rocha <lr359508@gmail.com>
2024-02-16 08:02:59 -0300

    refac: views code has been optimized

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -5,7 +5,6 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from authentication.serializers import UserSerializer
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
 from backend.settings import SECRET_KEY
 import jwt
 
@@ -25,20 +24,14 @@ def register_user(request):
 @permission_classes([AllowAny])
 def token_user(request):
     data = JSONParser().parse(request)
-
-    check = ('username' in data) and ('password' in data)
-
-    if check:
-        username = data['username']
-        password = data['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            encoded_jwt = jwt.encode({'sub':user.pk}, SECRET_KEY, algorithm="HS256")
-            return Response({'access_token':encoded_jwt},status=status.HTTP_201_CREATED)
-        else:
-            return Response({'Message':'Usuário não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
-    return Response({"Message":"Formato inválido."},status.HTTP_400_BAD_REQUEST)
+    username = data['username']
+    password = data['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        encoded_jwt = jwt.encode({'sub':user.pk}, SECRET_KEY, algorithm="HS256")
+        return Response({'access_token':encoded_jwt},status=status.HTTP_201_CREATED)
+    else:
+        return Response({'Message':'Usuário não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -46,9 +39,7 @@ def verify_user(request):
     data = JSONParser().parse(request)
     try:
         data_decoded = jwt.decode(data['access_token'],SECRET_KEY,algorithms=["HS256",])
-    except jwt.exceptions.DecodeError as e:
-        return Response({'Message':str(e)}, status.HTTP_400_BAD_REQUEST)
-    except KeyError as e:
+    except Exception as e:
         return Response({'Message':str(e)}, status.HTTP_400_BAD_REQUEST)
 
     return Response({**data_decoded}, status.HTTP_202_ACCEPTED)
