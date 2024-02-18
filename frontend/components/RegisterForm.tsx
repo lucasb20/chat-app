1a4693ea90c75cf285aebcc3238c0023509cfc97
Lucas Rocha <lr359508@gmail.com>
2024-02-18 15:20:30 -0300

    feat: RegisterForm implemented

--- frontend/components/RegisterForm.tsx
+++ frontend/components/RegisterForm.tsx
@@ -1,6 +1,44 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+
+interface RegisterData{
+    username: string,
+    password1: string,
+    password2: string
+}
 
 export default function RegisterForm(){
+    const [formData, setFormData] = useState<RegisterData>({username : "", password1 : "", password2 : ""})
+
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return(
-        <p>Not implemented</p>
+        <form onSubmit={handleSubmit}>
+            <section>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" placeholder="Enter your username" value={formData.username}
+                onChange={e=>setFormData({...formData, username : e.target.value})}
+                />
+            </section>
+            <section>
+                <label htmlFor="password1">Password:</label>
+                <input type="password" id="password1" placeholder="Enter your password" value={formData.password1}
+                onChange={e=>setFormData({...formData, password1 : e.target.value})}
+                />
+            </section>
+            <section>
+                <label htmlFor="password2">Confirm password:</label>
+                <input type="password" id="password2" placeholder="Enter your password again" value={formData.password2}
+                onChange={e=>setFormData({...formData, password2 : e.target.value})}
+                />
+            </section>
+            <section>
+                <button type="reset">Reset</button>
+                <button type="submit">Submit</button>
+            </section>
+        </form>
     )
 }
