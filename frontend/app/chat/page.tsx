b0e9cb6a284fa505cb05bea927a70fa966fbbf46
Lucas Rocha <lr359508@gmail.com>
2024-02-20 14:50:24 -0300

    fix: bad ChatForm loading

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -4,26 +4,28 @@ import ChatForm from "@/components/ChatForm";
 import { validateToken } from "@/services/APIService";
 import { TokenContext } from "@/services/AuthContext";
 import Link from "next/link";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import style from "@/app/page.module.css"
 
 export default function Page(){
     const context = useContext(TokenContext)
-    const [username, setUsername] = useState<string>("...")
+    const [username, setUsername] = useState<string | null>(null)
 
-    if(context !== undefined){
-        validateToken({ access_token : context.token })
-        .then(data => {
-            setUsername(data.username)
-        })
-    }
+    useEffect(() => {
+        if (context !== undefined && username === null) {
+            validateToken({ access_token : context.token })
+            .then(data => {
+                setUsername(data.username)
+            })
+        }
+    }, [username])
 
     return(
         <>
             <header><h1>Connected as {username}</h1></header>
 
             <main className={style.chat}>
-                <ChatForm username={username} />
+                {username && <ChatForm username={username}/>}
                 <nav>
                     <Link href="/">Back</Link>
                 </nav>
