6bec27f1abab8b8b1275d163cc79ea69eb0bec43
Lucas Rocha <lr359508@gmail.com>
2024-03-06 19:37:00 -0300

    fix: executable adjustment

--- scripts/install-depencies.py
+++ scripts/install-depencies.py
@@ -1,22 +1,25 @@
 
 import subprocess
+import sys
 
 def install_frontend():
-    process = subprocess.run(["npm", "install"], cwd="../frontend")
+    process = subprocess.run(["npm", "install"], cwd="../frontend", shell=True)
     return process.returncode
 
 def install_backend():
+    executable = sys.executable
+
     try:
-        subprocess.run(["py", "-m", "venv", "venv"], cwd="../backend", check=True)
-        subprocess.run(["py", "-m", "pip", "install", "-r", "requirements.txt"], cwd="../backend", check=True)
-        subprocess.run(["py", "manage.py", "migrate"], check=True)
+        subprocess.run([executable, "-m", "venv", "venv"], cwd="../backend", check=True)
+        subprocess.run([executable, "-m", "pip", "install", "-r", "requirements.txt"], cwd="../backend", check=True)
+        subprocess.run([executable, "manage.py", "migrate"], check=True)
     except Exception:
         exit(1)
 
     return 0
 
 def install_websocket_server():
-    process = subprocess.run(["npm", "install"], cwd="../websocket-server")
+    process = subprocess.run(["npm", "install"], cwd="../websocket-server", shell=True)
     return process.returncode
 
 
