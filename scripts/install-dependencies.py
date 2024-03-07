bb4bbfb30f3df7339b96fc18ea095cfd979602b0
Lucas Rocha <lr359508@gmail.com>
2024-03-06 22:15:32 -0300

    fix: path adjustments

--- scripts/install-dependencies.py
+++ scripts/install-dependencies.py
@@ -3,18 +3,19 @@ import os
 import subprocess
 import sys
 
+
 def install_frontend():
-    subprocess.run(["npm", "install"], cwd="../frontend", shell=True, check=True)
+    subprocess.run(["npm", "install"], cwd="frontend", shell=True, check=True)
 
 def install_backend():
     executable = sys.executable
     if os.name == 'posix':
         raise NotImplementedError("Not implemented for this OS.")
     elif os.name == 'nt':
-        venv_executable = "../backend/venv/Scripts/python.exe"
+        venv_executable = "backend/venv/Scripts/python.exe"
 
-    subprocess.run([executable, "-m", "venv", "venv"], cwd="../backend", check=True)
-    subprocess.run([venv_executable, "-m", "pip", "install", "-r", "requirements.txt"], cwd="../backend", check=True)
+    subprocess.run([executable, "-m", "venv", "venv"], cwd="backend", check=True)
+    subprocess.run([venv_executable, "-m", "pip", "install", "-r", "requirements.txt"], cwd="backend", check=True)
 
     data = """NAME_DB=name
 USER_DB=user
@@ -22,11 +23,11 @@ PASSWORD_DB=password
 HOST_DB=localhost
 PORT_DB=5432"""
 
-    with open("../backend/.env", "w") as env_file:
+    with open("backend/.env", "w") as env_file:
         env_file.write(data)
 
 def install_websocket_server():
-    subprocess.run(["npm", "install"], cwd="../websocket-server", shell=True, check=True)
+    subprocess.run(["npm", "install"], cwd="websocket-server", shell=True, check=True)
 
 
 if __name__ == "__main__":
--- scripts/run-servers.py
+++ scripts/run-servers.py
@@ -3,14 +3,15 @@ import subprocess
 
 
 def run_frontend():
-    subprocess.Popen(["npm", "run", "dev"], cwd="../frontend", shell=True)
+    subprocess.Popen(["npm", "run", "dev"], cwd="frontend", shell=True)
 
 def run_backend():
-    venv_executable = "../backend/venv/Scripts/python.exe"
-    subprocess.Popen([venv_executable, "manage.py", "runserver"], cwd="../backend")
+    venv_executable = "backend/venv/Scripts/python.exe"
+    subprocess.Popen([venv_executable, "manage.py", "runserver"], cwd="backend")
 
 def run_websocket_server():
-    subprocess.Popen(["npm", "run", "dev"], cwd="../websocket-server", shell=True)
+    subprocess.Popen(["npm", "run", "dev"], cwd="websocket-server", shell=True)
+
 
 if __name__ == "__main__":
     run_frontend()
