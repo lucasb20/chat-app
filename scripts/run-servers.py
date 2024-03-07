25f961fabdffdabbebbc9b72581001d2c4e78751
Lucas Rocha <lr359508@gmail.com>
2024-03-06 21:34:26 -0300

    feat: run-servers file created

---
+++ scripts/run-servers.py
@@ -0,0 +1,14 @@
+
+def run_frontend():
+    pass
+
+def run_backend():
+    pass
+
+def run_websocket_server():
+    pass
+
+if __name__ == "__main__":
+    run_frontend()
+    run_backend()
+    run_websocket_server()
