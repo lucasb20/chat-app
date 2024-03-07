38975311b74a3d239e1940d82b14b0ad6d461cc2
Lucas Rocha <lr359508@gmail.com>
2024-03-07 13:13:24 -0300

    feat: run-servers script working in posix SO

--- scripts/run-servers.py
+++ scripts/run-servers.py
@@ -1,16 +1,22 @@
 
+import os
 import subprocess
 
 
 def run_frontend():
-    subprocess.Popen(["npm", "run", "dev"], cwd="frontend", shell=True)
+    shell = os.name != 'posix'
+    subprocess.Popen(["npm", "run", "dev"], cwd="frontend", shell=shell)
 
 def run_backend():
-    venv_executable = "backend/venv/Scripts/python.exe"
+    if os.name == 'posix':
+        venv_executable = "venv/bin/python"
+    elif os.name == 'nt':
+        venv_executable = "backend/venv/Scripts/python.exe"
     subprocess.Popen([venv_executable, "manage.py", "runserver"], cwd="backend")
 
 def run_websocket_server():
-    subprocess.Popen(["npm", "run", "dev"], cwd="websocket-server", shell=True)
+    shell = os.name != 'posix'
+    subprocess.Popen(["npm", "run", "dev"], cwd="websocket-server", shell=shell)
 
 
 if __name__ == "__main__":
