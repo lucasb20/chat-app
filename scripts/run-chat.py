37ae59b1621e30c348b0cfc0b8b5fdac11ef22ad
Lucas Rocha <lr359508@gmail.com>
2024-03-08 09:48:12 -0300

    feat: login_user created

--- scripts/run-chat.py
+++ scripts/run-chat.py
@@ -13,11 +13,25 @@ def enter_login_page(driver : WebDriver):
             a.click()
             break
 
+def login_user(driver : WebDriver, username, password):
+    driver.implicitly_wait(5)
+    username_input = driver.find_element(By.ID, 'username')
+    password_input = driver.find_element(By.ID, 'password')
+
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+    
+    buttons = driver.find_elements(By.TAG_NAME, 'button')
+    for button in buttons:
+        if button.text == "Submit":
+            button.click()
+            break
+
 if __name__ == "__main__":
     driver = WebDriver()
     driver.get(APP_URL)
 
     enter_login_page(driver)
-    
+    login_user(driver, "lucasb20", "123")
 
     driver.quit()
