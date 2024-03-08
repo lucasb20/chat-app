48dc4825fa664b4140498369ccf713d1dfb377b4
Lucas Rocha <lr359508@gmail.com>
2024-03-08 09:31:41 -0300

    feat: enter_login_page created

--- scripts/run-chat.py
+++ scripts/run-chat.py
@@ -1,12 +1,23 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 APP_URL = "http://localhost:3000"
 
+def enter_login_page(driver : WebDriver):
+    menu = driver.find_element(By.TAG_NAME, 'ul')
+    buttons = menu.find_elements(By.TAG_NAME, 'li')
+
+    for element in buttons:
+        a = element.find_element(By.TAG_NAME, 'a')
+        if a.text == "Login":
+            a.click()
+            break
+
 if __name__ == "__main__":
     driver = WebDriver()
-    
     driver.get(APP_URL)
 
-    assert driver.title == "Raissa"
+    enter_login_page(driver)
+    
 
     driver.quit()
