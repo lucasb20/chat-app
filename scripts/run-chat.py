f9bc20915bbaedfd84ba62d5288d1b3dcf368537
Lucas Rocha <lr359508@gmail.com>
2024-03-09 13:58:52 -0300

    feat: functional test of chat created

--- scripts/run-chat.py
+++ scripts/run-chat.py
@@ -50,15 +50,31 @@ def send_in_chat(driver : WebDriver, message):
     driver.find_element(By.ID, 'send-button').click()
 
 if __name__ == "__main__":
-    driver = WebDriver()
-    driver.get(APP_URL)
+    driver1 = WebDriver()
+    driver2 = WebDriver()
+    
+    driver1.get(APP_URL)
+    driver2.get(APP_URL)
 
-    enter_login_page(driver)
+    enter_login_page(driver1)
+    enter_login_page(driver2)
+
     time.sleep(10)
-    login_user(driver, "lucasb20", "123")
+
+    login_user(driver1, "lucasb20", "123")
+    login_user(driver2, "Livia", "123")
+    
     time.sleep(10)
-    send_in_chat(driver, "Hi guys")
-    send_in_chat(driver, "How are you?")
+
+    send_in_chat(driver1, "Hey Livia, how's it going?")
+    send_in_chat(driver2, "Hi Lucas! I'm doing well, thanks for asking. How about you?")
+    send_in_chat(driver1, "I'm good, just chilling at home. Hey, did you catch the new episode of that show we were talking about?")
+    send_in_chat(driver2, "Oh yeah, I watched it last night! It was intense, wasn't it?")
+
     time.sleep(5)
 
-    driver.quit()
+    driver1.save_screenshot("other/lucas_vision.png")
+    driver2.save_screenshot("other/livia_vision.png")
+
+    driver1.quit()
+    driver2.quit()
