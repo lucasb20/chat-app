lucasb20/Raissa

feat: project created

init.sql
@@ -1,0 +1,1 @@
+CREATE DATABASE raissa_db;

lucasb20/chat-app

init.sql
@@ -1,0 +1,1 @@
+CREATE DATABASE my_db;